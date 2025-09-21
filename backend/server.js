8f70fed7ecfc31cfcc750b34716c53c7772be473
Abhinadhmt <mtabhinadh@gamil.com>
2025-09-21 12:06:12 +0530

    changes the databse to atles

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection
-mongoose.connect('mongodb://127.0.0.1:27017/foody', {
+// mongoose.connect('mongodb://127.0.0.1:27017/foody', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
